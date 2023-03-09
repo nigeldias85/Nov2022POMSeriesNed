5d04368d41414310a66e3baa14a0dfe7c5403f6d
nigeldias85 <nigeldias85@gmail.com>
2023-03-08 22:52:28 -0500

    Updated OrderPage code

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -5,10 +5,16 @@ import org.openqa.selenium.By;
 public class OrderPage {
 	
 	
-	By locator = By.id("order");
+	By orderLocator = By.id("order");
+	By pricingLocator = By.id("pricing");
 	
 	public void getOrder() {
 		System.out.println("Get Order.");
+	}
+	
+	
+	public void getPrice() {
+		System.out.println("Get Price.");
 	}
 
 }
