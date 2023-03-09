3f82b95ade9a20aef1965c6a481fc3f6e30ea2ce
nigeldias85 <nigeldias85@gmail.com>
2023-03-08 22:40:01 -0500

    Adding OrderPage code

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class OrderPage {
+	
+	
+	By locator = By.id("order");
+	
+	public void getOrder() {
+		System.out.println("Get Order.");
+	}
+
+}
