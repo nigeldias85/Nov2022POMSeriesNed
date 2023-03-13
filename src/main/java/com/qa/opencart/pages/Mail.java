bbd1f898aa95fbeb5f211e290318b2f52d781d7b
nigeldias85 <nigeldias85@gmail.com>
2023-03-12 23:48:10 -0400

    Mail method added

---
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -0,0 +1,20 @@
+package com.qa.opencart.pages;
+
+public class Mail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+	
+	public void mailSend() {
+	      System.out.println("Mail has been sent -- By Naveen");
+		}
+	
+	
+	public void getMail() {
+	      System.out.println("Mail has been received -- By Naveen");
+		}
+
+}
